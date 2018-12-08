d38fdc658d9ea4cb8227895ee30087977d1c75cb
shotgunfoot <sphillips93@outlook.com>
2018-12-08 13:52:24 +0000

    WindowPortals

---
+++ Assets/Scenes/PreyWindowPortal/FollowTransformWithOffset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransformWithOffset : MonoBehaviour {
+
+	public Transform Target;
+
+	public Transform OffsetPoint;
+
+	private void Update() {
+		transform.position = Target.position + OffsetPoint.position;
+		transform.rotation = Target.rotation;
+	}
+}
