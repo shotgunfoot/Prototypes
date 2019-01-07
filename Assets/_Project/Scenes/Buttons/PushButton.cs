938a77c87de87d1664483b46d1cf12af6ade754a
shotgunfoot <sphillips93@outlook.com>
2019-01-07 16:21:08 +0000

    A fancy button
    -Made a button model with a simple push animation in blender and imported to unity. For testing importing animations n materials n shtuff.

---
+++ Assets/_Project/Scenes/Buttons/PushButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushButton : MonoBehaviour
+{
+    private Animator animator;
+    public AnimationClip anim;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        animator.Play(anim.name);
+    }   
+}
