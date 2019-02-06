20492d295bc3d766733b1b2541aae4bef850003d
shotgunfoot <sphillips93@outlook.com>
2019-02-06 16:30:59 +0000

    More Gun Functionality
    Added a properties scriptable object to the gun so we can track things like projectiles per shot, shots per second, clip capacity etc.
    
    Then implemented basic reloading and firing system for the gun.
    
    Further additions can be gun spread, recoil, support for multiple shots etc.

--- Assets/_Project/Scenes/First Person Gun/Scripts/Gun.cs
+++ Assets/_Project/Scenes/First Person Gun/Scripts/Gun.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Gun : WieldableObject
 {
+    public WeaponProperties properties;
     public GameObject ProjectilePrefab;
     public Transform ProjectileOrigin;
     public float ProjectileForce;
     public AudioSource audio;
     public AudioClip clip;
 
+    public TextMeshProUGUI[] texts;
+
+    private bool reloading;
+    private bool canShoot;
+    private TimeSince timeSince;
+
     private void Start()
     {
-        audio = GetComponent<AudioSource>();    
+        audio = GetComponent<AudioSource>();
+        UpdateGunUI();
     }
 
     private void ShootProjectile()
@@ -20,11 +28,62 @@ public class Gun : WieldableObject
         GameObject obj = Instantiate(ProjectilePrefab, ProjectileOrigin.position, ProjectileOrigin.rotation);
         obj.GetComponent<Rigidbody>().AddForce(ProjectileOrigin.forward * ProjectileForce, ForceMode.Impulse);
         audio.PlayOneShot(clip);
+        properties.RemainingProjectilesInClip--;
     }
 
     public override void ObjectAction()
-    {
-        ShootProjectile();
+    {       
+        if(properties.RemainingProjectilesInClip > 0 && !reloading && timeSince > properties.ShotsPerSecond)
+        {
+            ShootProjectile();
+            timeSince = 0;
+        }
+        else if(properties.RemainingProjectilesInClip == 0)
+        {
+            if (!reloading)
+            {
+                reloading = true;
+                StartCoroutine(ReloadGun());
+            }            
+        }
+        else
+        {
+            ShootEmpty();
+        }
+        UpdateGunUI();
     }
-    
+
+    private void ShootEmpty()
+    {
+        //play gun empty firing noise.
+    }
+
+    private void UpdateGunUI()
+    {
+        for(int i = 0; i < texts.Length; i++)
+        {
+            texts[i].text = properties.RemainingProjectilesInClip + "/" + properties.TotalProjectilesRemaining;
+        }
+    }
+
+    /// <summary>
+    /// Basic reload functionality going on here, if the clip is empty attempt to fill it otherwise it never runs, cannot prematurely reload this gun.
+    /// </summary>
+    private IEnumerator ReloadGun()
+    {    
+        //we have enough to fill a clip so fill it and end early.
+        if(properties.TotalProjectilesRemaining > properties.ProjectilesPerClip)
+        {
+            properties.RemainingProjectilesInClip = properties.ProjectilesPerClip;
+            properties.TotalProjectilesRemaining -= properties.ProjectilesPerClip;                        
+        }
+        //we cant fill the clip but we can put whatevers left into it.
+        else if (properties.TotalProjectilesRemaining > 0)
+        {
+            properties.RemainingProjectilesInClip = properties.TotalProjectilesRemaining;
+            properties.TotalProjectilesRemaining = 0;            
+        }
+        yield return new WaitForSeconds(properties.ReloadTime);
+        reloading = false;
+    }
 }
---
+++ Assets/_Project/ScriptableObjects/WeaponProperties.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName ="WeaponProperties", menuName ="Weapons/Properties")]
+public class WeaponProperties : ScriptableObject
+{
+    public float NumberOfProjectilesPerShot;
+    public float ShotsPerSecond;
+    public float ProjectilesPerClip;
+    public float TotalProjectilesRemaining;
+    public float ReloadTime;
+    public float RemainingProjectilesInClip;
+}
