6341e940f0e81c1f97760f35676f8d6766e63a68
shotgunfoot <sphillips93@outlook.com>
2019-06-19 11:37:39 +0100

    Hit Reaction

---
+++ Assets/_Project/Scenes/First Person Fighting/HitReaction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class HitReaction : MonoBehaviour
+{
+
+    [SerializeField] private float forcePower = 5;
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void AddForceAtHitLocation(Vector3 hitPos, Vector3 hitDirection)
+    {
+        rb.AddForceAtPosition(hitDirection * forcePower, hitPos, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "PlayerWeapon")
+        {
+            Debug.Log("Adding Force!");
+            AddForceAtHitLocation(other.contacts[0].point, other.contacts[0].normal);
+        }
+
+    }
+}
+
