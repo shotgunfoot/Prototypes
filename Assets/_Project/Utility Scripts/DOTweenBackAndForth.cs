b99e0273b93ca52ca0a1cef72a914ea56d8818ab
shotgunfoot <sphillips93@outlook.com>
2019-08-19 13:24:27 +0100

    DOTween and Stencils

---
+++ Assets/_Project/Utility Scripts/DOTweenBackAndForth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DOTweenBackAndForth : MonoBehaviour
+{    
+    Sequence mySequence;
+
+    private void Start() {
+        mySequence = DOTween.Sequence();
+        mySequence.Append(transform.DOLocalMoveX(15, 2))
+        .Append(transform.DOLocalMoveX(-5, 2));
+        mySequence.SetLoops(-1);;
+        mySequence.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
